8bb2d8804070f2038ea73fe14482caffd61c7d67
Pawan Roy1997 <pawanroy0066@gmail.com>
2022-12-06 19:57:37 +0530

    Solution of: Isomorphic Strings

---
+++ src/test/kotlin/pawanroy/leetcode/isomorphic_strings/SolutionTest.kt
@@ -0,0 +1,15 @@
+package pawanroy.leetcode.isomorphic_strings
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class SolutionTest{
+    @Test
+    fun testCaseOne(){
+        assertTrue(Solution().isIsomorphic("add","egg"))
+    }
+    @Test
+    fun testCaseTwo(){
+        assertFalse(Solution().isIsomorphic("abcdefghijklmnopqrstuvwxyzva","abcdefghijklmnopqrstuvwxyzck"))
+    }
+}
