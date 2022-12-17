67df55d97e395eac24251af74876ba785f8b6b89
Pawan Roy1997 <pawanroy0066@gmail.com>
2022-12-17 20:43:18 +0530

    Climbing stairs

---
+++ src/test/kotlin/pawanroy/leetcode/climb_stairs/SolutionTest.kt
@@ -0,0 +1,27 @@
+package pawanroy.leetcode.climb_stairs
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class SolutionTest{
+
+    @Test
+    fun testCaseZero(){
+        assertEquals(0, Solution().climbStairs(0))
+    }
+
+    @Test
+    fun testCaseOne(){
+        assertEquals(1, Solution().climbStairs(1))
+    }
+
+    @Test
+    fun testCaseTwo(){
+        assertEquals(2, Solution().climbStairs(2))
+    }
+
+    @Test
+    fun testCaseThree(){
+        assertEquals(3, Solution().climbStairs(3))
+    }
+}
