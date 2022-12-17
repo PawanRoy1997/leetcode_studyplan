c07bcb45861c747bfaa20619ebf68a6dfd26e848
Pawan Roy1997 <pawanroy0066@gmail.com>
2022-12-17 17:37:16 +0530

    Coverage

--- src/main/kotlin/pawanroy/leetcode/flood_fill/Solution.kt
+++ src/main/kotlin/pawanroy/leetcode/flood_fill/Solution.kt
@@ -2,9 +2,7 @@ package pawanroy.leetcode.flood_fill
 
 class Solution {
     fun floodFill(image: Array<IntArray>, sr: Int, sc: Int, color: Int): Array<IntArray> {
-        if (image[sr][sc] != color) {
-            fillColor(image, image.size, image.first().size, sr, sc, image[sr][sc], color)
-        }
+        fillColor(image, image.size, image.first().size, sr, sc, image[sr][sc], color)
         return image
     }
 
@@ -17,11 +15,16 @@ class Solution {
         color: Int,
         newColor: Int
     ) {
-        if (sr < 0 || sr >= rows || sc < 0 || sc >= columns || image[sr][sc] != color) return
+        if (sr < 0) return
+        if (sr >= rows) return
+        if (sc < 0) return
+        if (sc >= columns) return
+        if (image[sr][sc] != color) return
+        if (image[sr][sc] == newColor) return
         image[sr][sc] = newColor
-        fillColor(image, rows, columns, sr-1, sc, color, newColor)
-        fillColor(image, rows, columns, sr+1, sc, color, newColor)
-        fillColor(image, rows, columns, sr, sc-1, color, newColor)
-        fillColor(image, rows, columns, sr, sc+1, color, newColor)
+        fillColor(image, rows, columns, sr - 1, sc, color, newColor)
+        fillColor(image, rows, columns, sr + 1, sc, color, newColor)
+        fillColor(image, rows, columns, sr, sc - 1, color, newColor)
+        fillColor(image, rows, columns, sr, sc + 1, color, newColor)
     }
 }
--- src/test/kotlin/pawanroy/leetcode/flood_fill/SolutionTest.kt
+++ src/test/kotlin/pawanroy/leetcode/flood_fill/SolutionTest.kt
@@ -5,23 +5,32 @@ import org.junit.jupiter.api.Test
 import java.util.*
 
 class SolutionTest {
+    private val input = arrayOf(
+        intArrayOf(1, 1, 1),
+        intArrayOf(1, 1, 2),
+        intArrayOf(1, 0, 1)
+    )
+    private val expected = arrayOf(
+        intArrayOf(2, 2, 2),
+        intArrayOf(2, 2, 2),
+        intArrayOf(2, 0, 1)
+    )
+
     @Test
     fun testCaseOne() {
-        val input = arrayOf(
-            intArrayOf(1, 1, 1),
-            intArrayOf(1, 1, 0),
-            intArrayOf(1, 0, 1)
-        )
-        val expected = arrayOf(
-            intArrayOf(2, 2, 2),
-            intArrayOf(2, 2, 0),
-            intArrayOf(2, 0, 1)
-        )
-
         val output = Solution().floodFill(input, 1, 1, 2)
         expected.forEachIndexed{
             index,array ->
             assertTrue(Arrays.equals(array, output[index]))
+        }
+    }
+
+    @Test
+    fun testCaseTwo() {
+        val output = Solution().floodFill(input, 1, 1, 1)
+        input.forEachIndexed{
+            index,array ->
+            assertTrue(array.contentEquals(output[index]))
         }
     }
 }
