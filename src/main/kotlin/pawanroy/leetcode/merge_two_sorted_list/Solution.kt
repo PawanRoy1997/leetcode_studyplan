5147eb763d68332c2b95b3e0874e6476e20c36f9
PawanRoy1997 <pawanroy0066@gmail.com>
2022-12-26 19:45:59 +0530

    Speed

--- src/main/kotlin/pawanroy/leetcode/merge_two_sorted_list/Solution.kt
+++ src/main/kotlin/pawanroy/leetcode/merge_two_sorted_list/Solution.kt
@@ -6,7 +6,7 @@ class Solution {
     fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
         if (list2 == null) return list1
         if (list1 == null) return list2
-        return if (list1.`val` > list2.`val`) list2.apply { next = mergeTwoLists(list1, list2.next) }
-        else list1.apply { next = mergeTwoLists(list1.next, list2) }
+        return if (list1.`val` > list2.`val`) list2.also { list2.next = mergeTwoLists(list1, list2.next) }
+        else list1.also { list1.next = mergeTwoLists(list1.next, list2) }
     }
 }
