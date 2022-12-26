01b9049d758ae3e6ce90b865b2daf4a832084bdd
PawanRoy1997 <pawanroy0066@gmail.com>
2022-12-26 19:38:20 +0530

    Speed

--- src/main/kotlin/pawanroy/leetcode/isomorphic_strings/Solution.kt
+++ src/main/kotlin/pawanroy/leetcode/isomorphic_strings/Solution.kt
@@ -1,37 +1,22 @@
 package pawanroy.leetcode.isomorphic_strings
 
 class Solution {
-
-    private val alphabets = arrayOf(
-        "a", "b", "c", "d",
-        "e", "f", "g", "h",
-        "i", "j", "k", "l",
-        "m", "n", "o", "p",
-        "q", "r", "s", "t",
-        "u", "v", "w", "x",
-        "y", "z"
-    )
-
     fun isIsomorphic(s: String, t: String): Boolean {
-        val sMap = s.uniqueValueMap()
-        val tMap = t.uniqueValueMap()
-        return sMap.equals(tMap, true)
-    }
+        val mapS = IntArray(256){-1}
+        val mapT = IntArray(256){-1}
 
-    private fun String.uniqueValueMap(): String {
-        var index = 0
-        val map: MutableMap<Int, Int> = HashMap()
-        this.chars().distinct().forEach {
-            map[it] = index
-            index++
-        }
-        val result = StringBuilder()
-        this.chars().forEach {
-            var ind = map[it]!!
-            ind = if(ind > 25) ind % 25 else ind
-            result.append(alphabets[ind])
-        }
-        return result.toString()
-    }
+        for(i in s.indices) {
+            val c1: Int = s[i].code
+            val c2: Int = t[i].code
 
+            if(mapS[c1] == -1 && mapT[c2] == -1) {
+                mapS[c1] = c2
+                mapT[c2] = c1
+            } else if(!(mapS[c1] == c2 && mapT[c2] ==
+                        c1)) {
+                return false
+            }
+        }
+        return true
+    }
 }
