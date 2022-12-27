609d17eba087edb7276642daa7dac4c20e2a9e92
PawanRoy1997 <pawanroy0066@gmail.com>
2022-12-27 18:46:32 +0530

    Speed

--- src/main/kotlin/pawanroy/leetcode/reverse_linked_list/Solution.kt
+++ src/main/kotlin/pawanroy/leetcode/reverse_linked_list/Solution.kt
@@ -5,8 +5,16 @@ import pawanroy.leetcode.ListNode
 class Solution {
 
     fun reverseList(head: ListNode?): ListNode? {
-        if (head == null) return head
-        return getTail(null, head)
+        if (head == null) return null
+        var result: ListNode? = null
+        var next:ListNode? = head
+        while (next != null) {
+            val swap = next
+            next = next.next
+            swap.next = result
+            result = swap
+        }
+        return result
     }
 
     private fun getTail(prev: ListNode?, current: ListNode): ListNode? {
