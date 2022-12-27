240194bfca7514d9841cf092dde4205d0470309d
PawanRoy1997 <pawanroy0066@gmail.com>
2022-12-27 19:01:04 +0530

    Speed

--- src/main/kotlin/pawanroy/leetcode/middle_of_linked_list/Solution.kt
+++ src/main/kotlin/pawanroy/leetcode/middle_of_linked_list/Solution.kt
@@ -5,14 +5,12 @@ import pawanroy.leetcode.ListNode
 class Solution {
 
     fun middleNode(head: ListNode?): ListNode? {
-        if (head == null) return head
-        val half = head.length()
-        var next = head
-        repeat(half / 2) { next = next!!.next }
-        return if (half % 2 == 0) next else next!!.next
-    }
-
-    private fun ListNode.length(size: Int = 0): Int {
-        return if (this.next != null) this.next!!.length(size + 1) else size
+        var cursor: ListNode? = head
+        var half = cursor
+        while (cursor?.next != null) {
+            cursor = cursor.next!!.next ?: cursor.next
+            half = half!!.next
+        }
+        return half
     }
 }
