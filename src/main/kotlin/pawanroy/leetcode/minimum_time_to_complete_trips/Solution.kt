0f010d15600917d7043d4e9d6657a9de047ee190
PawanRoy <pawanroy0066@gmail.com>
2023-03-07 22:19:34 +0530

    Minimum time to complete trips

---
+++ src/main/kotlin/pawanroy/leetcode/minimum_time_to_complete_trips/Solution.kt
@@ -0,0 +1,44 @@
+package pawanroy.leetcode.minimum_time_to_complete_trips
+
+class Solution {
+    fun minimumTime(time: IntArray, totalTrips: Int): Long {
+
+        var maxTime = 0
+        var tripsInMaxTime = 0
+        var minTime = time[0]
+
+        for (t in time) {
+            if (maxTime < t) maxTime = t
+            if (minTime > t) minTime = t
+        }
+
+        for (t in time) {
+            tripsInMaxTime += maxTime / t
+        }
+
+        var upperBound = totalTrips.toLong() * maxTime
+        var lowerBound: Long = minTime.toLong()
+        var trips: Long
+        var mid: Long
+        while (true) {
+            trips = 0L
+            if (upperBound == lowerBound) return lowerBound
+            if (upperBound - lowerBound == 1L) {
+                for (t in time) {
+                    trips += lowerBound / t
+                }
+                return if (trips.toInt() >= totalTrips) {
+                    lowerBound
+                } else upperBound
+            }
+
+            mid = lowerBound + (upperBound - lowerBound) / 2
+            for (t in time) {
+                trips += mid / t
+            }
+
+            if (trips >= totalTrips.toLong()) upperBound = mid
+            if (trips < totalTrips.toLong()) lowerBound = mid
+        }
+    }
+}
