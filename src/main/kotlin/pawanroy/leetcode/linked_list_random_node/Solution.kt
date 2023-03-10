d4e94b28f3893e1bd6db9cb1f4f994b6dabd4d74
PawanRoy <pawanroy0066@gmail.com>
2023-03-10 10:56:46 +0530

    Random node from linked list

---
+++ src/main/kotlin/pawanroy/leetcode/linked_list_random_node/Solution.kt
@@ -0,0 +1,22 @@
+package pawanroy.leetcode.linked_list_random_node
+
+import pawanroy.leetcode.ListNode
+
+class Solution(head: ListNode?) {
+    private var headPtr = head
+    private var input = ArrayList<Int>()
+    private var size = 0
+
+    init {
+        while (headPtr != null) {
+            input.add(headPtr!!.`val`)
+            headPtr = headPtr?.next
+        }
+        size = input.size
+    }
+
+    fun getRandom(): Int {
+        return input[kotlin.random.Random.nextInt(size)]
+    }
+
+}
