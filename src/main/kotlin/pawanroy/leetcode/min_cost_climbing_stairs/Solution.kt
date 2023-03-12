8a97d5122d3a5b4e432e2c14483a99a773faa7f7
PawanRoy <pawanroy0066@gmail.com>
2023-03-12 15:46:44 +0530

    min cost climbing stairs

--- src/main/kotlin/pawanroy/leetcode/min_cost_climbing_stairs/Solution.kt
+++ src/main/kotlin/pawanroy/leetcode/min_cost_climbing_stairs/Solution.kt
@@ -1,22 +1,10 @@
 package pawanroy.leetcode.min_cost_climbing_stairs
 
+import kotlin.math.min
+
 class Solution {
     fun minCostClimbingStairs(cost: IntArray): Int {
-        var cursor = -1
-        var totalCost = 0
-        while (cursor < cost.size) {
-            if (cursor == cost.lastIndex) break
-            if (cursor == cost.lastIndex-1) break
-            val sides = cost[cursor + 1] + if (cost.size > cursor + 3) cost[cursor + 3] else 0
-            val middle = if (cursor + 2 < cost.size) cost[cursor + 2] else 0
-            totalCost = if (middle < sides) {
-                cursor += 2
-                totalCost + cost[cursor]
-            } else {
-                cursor += 3
-                sides
-            }
-        }
-        return totalCost
+        for (i in 2 until cost.size) cost[i] = min(cost[i - 1], cost[i - 2]) + cost[i]
+        return min(cost[cost.size - 1], cost[cost.size - 2])
     }
 }
