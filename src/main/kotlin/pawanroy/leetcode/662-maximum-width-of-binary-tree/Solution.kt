12783be905c26ee0df7a243f13db87e7e90b4633
Pawan Roy <pawanroy0066@gmail.com>
2023-04-21 21:00:39 +0530

    maximum width of a binary tree

---
+++ src/main/kotlin/pawanroy/leetcode/662-maximum-width-of-binary-tree/Solution.kt
@@ -0,0 +1,31 @@
+package pawanroy.leetcode.`662-maximum-width-of-binary-tree`
+
+import pawanroy.leetcode.TreeNode
+import java.util.*
+
+class Solution {
+    class Node(var node: TreeNode, var idx: Int)
+
+    @Suppress("kotlin:S3776")
+    fun widthOfBinaryTree(root: TreeNode?): Int {
+        if (root == null) return 0
+        val queue: Queue<Node> = LinkedList()
+        queue.add(Node(root, 0))
+        var max = 0
+        while (!queue.isEmpty()) {
+            val size = queue.size
+            var start = 0
+            var end = 0
+            for (i in 0 until size) {
+                val eachNode = queue.remove()
+                val index = eachNode.idx
+                if (i == 0) start = index //start and end index for each level
+                if (i == size - 1) end = index
+                if (eachNode.node.left != null) queue.add(Node(eachNode.node.left!!, 2 * eachNode.idx))
+                if (eachNode.node.right != null) queue.add(Node(eachNode.node.right!!, 2 * eachNode.idx + 1))
+            }
+            max = maxOf(max, end - start + 1)
+        }
+        return max
+    }
+}
