4c5ba0dd01324acfeccdd37c1fe4e3c7a2fd32cd
Pawan Roy <pawanroy0066@gmail.com>
2023-05-08 21:24:56 +0530

    Bulb switcher solved

--- src/main/kotlin/pawanroy/leetcode/319-bulb-switcher/Solution.kt
+++ src/main/kotlin/pawanroy/leetcode/319-bulb-switcher/Solution.kt
@@ -2,18 +2,13 @@ package pawanroy.leetcode.`319-bulb-switcher`
 
 class Solution {
     fun bulbSwitch(n: Int): Int {
-        var l = 1L
-        var r: Long = n.toLong()
-        while (l <= r) {
-            val mid = l + (r - l) / 2
-            if (mid * mid == n.toLong()) {
-                return mid.toInt()
-            } else if (mid * mid < n.toLong()) {
-                l = mid + 1
-            } else {
-                r = mid - 1
-            }
+        var sum = -1
+        val longN = n.toLong()
+        for (i in 0..Int.MAX_VALUE) {
+            val long = i.toLong()
+            if (longN < long * long) break
+            else sum++
         }
-        return r.toInt()
+        return sum
     }
 }
