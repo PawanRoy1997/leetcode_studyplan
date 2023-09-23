8f8738d152af07f9eea042bc5df77d9e43ded8a9
Pawan <pawanroy0066@gmail.com>
2023-09-23 09:14:44 +0530

    643 maximum average sub-array

--- src/main/kotlin/pawanroy/leetcode/643-maximum-average-subarray/Solution.kt
+++ src/main/kotlin/pawanroy/leetcode/643-maximum-average-subarray/Solution.kt
@@ -4,7 +4,25 @@ class Solution {
     fun findMaxAverage(nums: IntArray, k: Int): Double {
         val shiftSpace = nums.size - k
         var average: Double = 0.0
+        // Meta data
+        val leftSum = Array(shiftSpace){ nums[it] }
+        val rightSum = Array(shiftSpace){ nums[k+it] }
+        for(i in 1 until shiftSpace){
+            leftSum[i]+=leftSum[i-1]
+            rightSum[rightSum.lastIndex-i]+=rightSum[rightSum.lastIndex-i+1]
+        }
 
-        return average
+        var startPtr = shiftSpace-1
+        repeat(shiftSpace) {
+            // Chose correct start and end
+            if (startPtr != leftSum.size && leftSum[startPtr] < rightSum[startPtr]) startPtr++
+            else if(startPtr != 0) startPtr--
+        }
+
+        // Find average
+        for(i in 0 until k){
+            average+=nums[startPtr+i]
+        }
+        return average/k
     }
 }
