5c35fd4792be8c49fc5154635693addea9f5a5d3
Pawan Roy <pawan@deltafour.co>
2025-07-22 09:17:03 +0530

    1695-maximum-erasure_value

--- src/main/kotlin/pawanroy/leetcode/1695-maximum-erasure_value/Solution.kt
+++ src/main/kotlin/pawanroy/leetcode/1695-maximum-erasure_value/Solution.kt
@@ -1,19 +1,21 @@
 package pawanroy.leetcode.`1695-maximum-erasure_value`
 
-import java.util.LinkedList
-import java.util.Queue
-
 class Solution {
     fun maximumUniqueSubarray(nums: IntArray): Int {
-        val subArray: Queue<Int> = LinkedList()
         var score = 0
+        var ptr = 0
         var result = 0
-        for(i in nums.indices){
-            while(subArray.contains(nums[i])){
-                score -= subArray.poll()
+        var subArray = mutableSetOf<Int>()
+
+        for (i in nums.indices) {
+            while(subArray.contains(nums[i])) {
+                subArray.remove(nums[ptr])
+                score -= nums[ptr]
+                ptr++
             }
+
+            subArray.add(nums[i])
             score+= nums[i]
-            subArray.offer(nums[i])
             result = maxOf(result, score)
         }
 
