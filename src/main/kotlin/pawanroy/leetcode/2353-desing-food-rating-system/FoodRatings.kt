2ab6050fc11a8fd26940496a9a56e7051e8e08c9
PawanRoy <pawanroy0066@gmail.com>
2025-09-17 09:09:39 +0530

    2353-desing-food-rating-system

--- src/main/kotlin/pawanroy/leetcode/2353-desing-food-rating-system/FoodRatings.kt
+++ src/main/kotlin/pawanroy/leetcode/2353-desing-food-rating-system/FoodRatings.kt
@@ -1,30 +1,54 @@
 package pawanroy.leetcode.`2353-desing-food-rating-system`
 
-import java.util.*
+
+import java.util.PriorityQueue
 
 class FoodRatings(foods: Array<String>, cuisines: Array<String>, ratings: IntArray) {
+
     data class FoodItem(val name: String, val cuisine: String, var rating: Int)
 
-    private val foodComparator = compareByDescending<FoodItem> { it.rating }
+
+    private val comparator = compareByDescending<FoodItem> { it.rating }
         .thenBy { it.name }
 
-    private val foodList: PriorityQueue<FoodItem> = PriorityQueue(foodComparator)
+
+    private val foodMap = mutableMapOf<String, FoodItem>()
+
+
+    private val cuisineMap = mutableMapOf<String, PriorityQueue<FoodItem>>()
 
     init {
-        foods.forEachIndexed { index, foodItem ->
-            foodList.add(FoodItem(foodItem, cuisines[index], ratings[index]))
+        for (i in foods.indices) {
+            val food = FoodItem(foods[i], cuisines[i], ratings[i])
+            foodMap[foods[i]] = food
+            cuisineMap.computeIfAbsent(cuisines[i]) { PriorityQueue(comparator) }
+                .add(food)
         }
-        println(foodList)
     }
 
     fun changeRating(food: String, newRating: Int) {
-        val item = foodList.first { it.name == food }
-        foodList.remove(item)
-        foodList.offer(item.copy(rating = newRating))
-        println(foodList)
+        val item = foodMap[food]!!
+        val updated = FoodItem(item.name, item.cuisine, newRating)
+
+
+        foodMap[food] = updated
+
+
+        cuisineMap[item.cuisine]!!.add(updated)
     }
 
     fun highestRated(cuisine: String): String {
-        return foodList.first { it.cuisine == cuisine }.name
+        val pq = cuisineMap[cuisine]!!
+
+
+        while (true) {
+            val top = pq.peek()
+            val latest = foodMap[top.name]!!
+            if (top.rating == latest.rating) {
+                return top.name
+            } else {
+                pq.poll()
+            }
+        }
     }
 }
